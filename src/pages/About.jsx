b4ef65a9384511c5bf162e05f80a5e2f078bd9dc
pro-mannan <promasudbd@gmail.com>
2023-11-25 23:58:04 +0600

    react js useState method working with couter and handle function

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react"
 import Header from "../components/Header"
 
 const About = () => {
+  const [couter, setCouter] = useState(0);
+  const handleCouterInc = () => {
+    setCouter((prevState) => prevState + 1);
+  }
   return (
     <>
         <Header />
         <h1>About Us</h1>
+        <hr />
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-md-6">
+              <div className="card">
+                <div className="card-body">
+                  <h2 style={{fontSize: "80px", color:"red", fontWeight: "bold"}}>{couter}</h2>
+                  <hr />
+                  <button className="btn btn-danger mx-2">--</button>
+                  <button className="btn btn-info" onClick={handleCouterInc}>++</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
     </>
   )
 }
