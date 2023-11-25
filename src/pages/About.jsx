61f6c3cc5a26849bb5a8bcb0e05054af09e67b06
pro-mannan <promasudbd@gmail.com>
2023-11-26 00:23:42 +0600

    multipul color Choose handlefunction create inline

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,6 +3,7 @@ import Header from "../components/Header"
 
 const About = () => {
   const [couter, setCouter] = useState(0);
+  const [bgColor, setBgColor] = useState("red");
   const handleCouterDec = () => {
     setCouter((prevState) => prevState -1);
   }
@@ -17,7 +18,7 @@ const About = () => {
         <Header />
         <h1>About Us</h1>
         <hr />
-        <div className="container">
+        <div className="container p-5" style={{backgroundColor: bgColor}}>
           <div className="row justify-content-center">
             <div className="col-md-6">
               <div className="card">
@@ -27,6 +28,13 @@ const About = () => {
                   <button className="btn btn-danger mx-2" onClick={handleCouterDec}>--</button>
                   <button className="btn btn-info" onClick={handleCouterInc}>++</button>
                   <button className="btn btn-info mx-2" onClick={handleCouterReset}>Reset</button>
+                  <hr />
+                  <button onClick={() => setBgColor('red')}>Red</button>
+                  <button onClick={() => setBgColor('green')}>Green</button>
+                  <button onClick={() => setBgColor('skyblue')}>SkyBlue</button>
+                  <button onClick={() => setBgColor('blue')}>Blue</button>
+                  <button onClick={() => setBgColor('orange')}>Orange</button>
+                  <button onClick={() => setBgColor('pink')}>Pink</button>
                 </div>
               </div>
             </div>
